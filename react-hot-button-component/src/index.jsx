b35338ca51928c1bdbe0af1fe128df381ac13ac6
Youssef Najjarine <youssefnajjarine@hotmail.com>
2021-10-13 15:14:56 -0700

    added more functionality to getClassName method.

--- react-hot-button-component/src/index.jsx
+++ react-hot-button-component/src/index.jsx
@@ -14,17 +14,17 @@ class HotButton extends React.Component {
   }
 
   getClassName() {
-    return this.state.incrementor === 3
+    return this.state.incrementor > 2 && this.state.incrementor < 6
       ? 'blue'
-      : this.state.incrementor === 6
+      : this.state.incrementor > 5 && this.state.incrementor < 9
         ? 'lightBlue'
-        : this.state.incrementor === 9
+        : this.state.incrementor > 8 && this.state.incrementor < 12
           ? 'red'
-          : this.state.incrementor === 12
+          : this.state.incrementor > 11 && this.state.incrementor < 15
             ? 'orange'
-            : this.state.incrementor === 15
+            : this.state.incrementor > 14 && this.state.incrementor < 18
               ? 'yellow'
-              : this.state.incrementor === 18
+              : this.state.incrementor >= 18
                 ? 'white'
                 : 'none';
   }
