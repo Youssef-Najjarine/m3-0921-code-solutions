817492293e3daf94c82814acdbeb1e23fa4500c4
Youssef Najjarine <youssefnajjarine@hotmail.com>
2021-10-13 14:13:34 -0700

    Completed react-hot-button-component.

---
+++ react-hot-button-component/src/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class HotButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { incrementor: 0 };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const click = this.state.incrementor;
+    this.setState({ incrementor: click + 1 });
+  }
+
+  render() {
+    return (
+      <button className={
+        this.state.incrementor === 3
+          ? 'blue'
+          : this.state.incrementor === 6
+            ? 'lightBlue'
+            : this.state.incrementor === 9
+              ? 'red'
+              : this.state.incrementor === 12
+                ? 'orange'
+                : this.state.incrementor === 15
+                  ? 'yellow'
+                  : this.state.incrementor === 18
+                    ? 'white'
+                    : 'none'
+      } onClick={this.handleClick}>Hot Button</button>
+    );
+  }
+}
+
+ReactDOM.render(
+  <HotButton/>,
+  document.getElementById('root')
+);
