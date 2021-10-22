d197ca95e4c773852eee606ec6aa6dd3c9cce134
Youssef Najjarine <youssefnajjarine@hotmail.com>
2021-10-21 21:40:36 -0700

    Component now takes in an array of topics and creates butons and content according to how many are in array.

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -4,105 +4,38 @@ class Accordion extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currenButtonDisplayed: ''
+      activeId: 0
     };
-    this.handleHTMLButton = this.handleHTMLButton.bind(this);
-    this.handleCSSButton = this.handleCSSButton.bind(this);
-    this.handleJSButton = this.handleJSButton.bind(this);
+    this.topics = props.topics;
+    this.handleButtonClicks = this.handleButtonClicks.bind(this);
   }
 
-  handleHTMLButton(event) {
-    if (event.target.className === 'active') {
-      event.target.className = '';
-      this.setState({ currenButtonDisplayed: '' });
-    } else {
-      event.target.className = 'active';
-      this.setState({ currenButtonDisplayed: 'HML' });
-    }
+  handleButtonClicks(id) {
+    return this.state.activeId === id
+      ? this.setState({ activeId: 0 })
+      : this.setState({ activeId: id });
   }
 
-  handleCSSButton(event) {
-    if (event.target.className === 'active') {
-      event.target.className = '';
-      this.setState({ currenButtonDisplayed: '' });
-    } else {
-      event.target.className = 'active';
-      this.setState({ currenButtonDisplayed: 'CSS' });
-    }
+  handleTopicsDisplayed() {
+    const topicItems = this.topics.map(topic =>
+      <div key={topic.id}>
+        <button onClick={() => this.handleButtonClicks(topic.id)}>{topic.title}</button>
+        <p className={this.handleContentDisplayed(topic.id)}>{topic.content}</p>
+      </div>
+    );
+    return topicItems;
   }
 
-  handleJSButton(event) {
-    if (event.target.className === 'active') {
-      event.target.className = '';
-      this.setState({ currenButtonDisplayed: '' });
-    } else {
-      event.target.className = 'active';
-      this.setState({ currenButtonDisplayed: 'JS' });
-    }
-  }
-
-  handleHypertextMarkupLanguageText() {
-    return this.state.currenButtonDisplayed === 'HML'
-      ? ''
-      : 'hidden';
-  }
-
-  handleCascadingStyleSheetsText() {
-    return this.state.currenButtonDisplayed === 'CSS'
-      ? ''
-      : 'hidden';
-  }
-
-  handleJavaScriptText() {
-    return this.state.currenButtonDisplayed === 'JS'
+  handleContentDisplayed(id) {
+    return this.state.activeId === id
       ? ''
       : 'hidden';
   }
 
   render() {
     return (
-      <section id="container">
-        <div id="buttonsAndText">
-          <button onClick={this.handleHTMLButton}>Hypertext Markup Language</button>
-          <p className={this.handleHypertextMarkupLanguageText()}>
-            typesetting, remaining essentially unchanged.
-             It was popularised in the 1960s with the release
-             of Letraset sheets containing Lorem Ipsum passages,
-             and more recently with desktop publishing software
-             like Aldus PageMaker including versions of Lorem Ipsum.
-            typesetting, remaining essentially unchanged.
-            It was popularised in the 1960s with the release
-            of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software
-            like Aldus PageMaker including versions of Lorem Ipsum.
-             </p>
-          <button onClick={this.handleCSSButton}>Cascading Style Sheets</button>
-          <p className={this.handleCascadingStyleSheetsText()}>
-            typesetting, remaining essentially unchanged.
-            It was popularised in the 1960s with the release
-            of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software
-            like Aldus PageMaker including versions of Lorem Ipsum.
-            typesetting, remaining essentially unchanged.
-            It was popularised in the 1960s with the release
-            of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software
-            like Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
-          <button onClick={this.handleJSButton}>JavaScript</button>
-          <p className={this.handleJavaScriptText()}>
-            typesetting, remaining essentially unchanged.
-            It was popularised in the 1960s with the release
-            of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software
-            like Aldus PageMaker including versions of Lorem Ipsum.
-            typesetting, remaining essentially unchanged.
-            It was popularised in the 1960s with the release
-            of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software
-            like Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
-        </div>
+      <section className="container">
+        <div className="buttonsAndText">{this.handleTopicsDisplayed()}</div>
       </section>
     );
   }
--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Accordion from './accordion';
+
+const topics = [
+  {
+    id: 1,
+    title: 'HTML',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus id impedit repellendus aliquid cum expedita, quis exercitationem repudiandae dicta atque a unde? Quidem, cupiditate magni. Dignissimos, repellendus reprehenderit. Nihil, asperiores.'
+  },
+  {
+    id: 2,
+    title: 'CSS3',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus id impedit repellendus aliquid cum expedita, quis exercitationem repudiandae dicta atque a unde? Quidem, cupiditate magni. Dignissimos, repellendus reprehenderit. Nihil, asperiores.'
+  },
+  {
+    id: 3,
+    title: 'JavaScript',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus id impedit repellendus aliquid cum expedita, quis exercitationem repudiandae dicta atque a unde? Quidem, cupiditate magni. Dignissimos, repellendus reprehenderit. Nihil, asperiores.'
+  }
+
+];
 ReactDOM.render(
-  <Accordion/>,
+  <Accordion topics={topics}/>,
   document.getElementById('root')
 );
