4e122c505db24c5801a7ebda2e92076116260a48
Youssef Najjarine <youssefnajjarine@hotmail.com>
2021-10-22 21:24:17 -0700

    images now come in through props and CSS has been cleaned up.

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -3,249 +3,209 @@ import React from 'react';
 class Carousel extends React.Component {
   constructor(props) {
     super(props);
+    this.pictures = props.pictures;
     this.state = {
-      currentPicture: 1
+      incrementor: 0
     };
+
     this.timer = 0;
     this.handleRightAngle = this.handleRightAngle.bind(this);
     this.handleLeftAngle = this.handleLeftAngle.bind(this);
-    this.handleDot1Click = this.handleDot1Click.bind(this);
-    this.handleDot2Click = this.handleDot2Click.bind(this);
-    this.handleDot3Click = this.handleDot3Click.bind(this);
-    this.handleDot4Click = this.handleDot4Click.bind(this);
-    this.handleDot5Click = this.handleDot5Click.bind(this);
+    this.handleDotClick = this.handleDotClick.bind(this);
   }
 
   componentDidMount() {
     this.timer = setInterval(() => {
-      return this.state.currentPicture === 1
-        ? this.setState({ currentPicture: 2 })
-        : this.state.currentPicture === 2
-          ? this.setState({ currentPicture: 3 })
-          : this.state.currentPicture === 3
-            ? this.setState({ currentPicture: 4 })
-            : this.state.currentPicture === 4
-              ? this.setState({ currentPicture: 5 })
-              : this.state.currentPicture === 5
-                ? this.setState({ currentPicture: 1 })
-                : 1;
+      if (this.state.incrementor === this.pictures.length - 1) {
+        this.setState({ incrementor: 0 });
+      } else {
+        this.setState({ incrementor: this.state.incrementor + 1 });
+      }
     }, 3000);
-  }
-
-  handlePicture() {
-    return this.state.currentPicture === 1
-      ? 'images/1.png'
-      : this.state.currentPicture === 2
-        ? 'images/2.png'
-        : this.state.currentPicture === 3
-          ? 'images/3.png'
-          : this.state.currentPicture === 4
-            ? 'images/4.png'
-            : this.state.currentPicture === 5
-              ? 'images/5.png'
-              : '';
   }
 
   handleLeftAngle() {
     clearInterval(this.timer);
-    if (this.state.currentPicture === 1) {
-      this.setState({ currentPicture: 5 });
-    } else if (this.state.currentPicture === 2) {
-      this.setState({ currentPicture: 1 });
-    } else if (this.state.currentPicture === 3) {
-      this.setState({ currentPicture: 2 });
-    } else if (this.state.currentPicture === 4) {
-      this.setState({ currentPicture: 3 });
-    } else if (this.state.currentPicture === 5) {
-      this.setState({ currentPicture: 4 });
+    if (this.state.incrementor === 0) {
+      this.setState({ incrementor: this.pictures.length - 1 });
+    } else {
+      this.setState({ incrementor: this.state.incrementor - 1 });
     }
     this.timer = setInterval(() => {
-      return this.state.currentPicture === 1
-        ? this.setState({ currentPicture: 2 })
-        : this.state.currentPicture === 2
-          ? this.setState({ currentPicture: 3 })
-          : this.state.currentPicture === 3
-            ? this.setState({ currentPicture: 4 })
-            : this.state.currentPicture === 4
-              ? this.setState({ currentPicture: 5 })
-              : this.state.currentPicture === 5
-                ? this.setState({ currentPicture: 1 })
-                : 1;
+      if (this.state.incrementor === this.pictures.length - 1) {
+        this.setState({ incrementor: 0 });
+      } else {
+        this.setState({ incrementor: this.state.incrementor + 1 });
+      }
     }, 3000);
   }
 
   handleRightAngle() {
     clearInterval(this.timer);
-    if (this.state.currentPicture === 1) {
-      this.setState({ currentPicture: 2 });
-    } else if (this.state.currentPicture === 2) {
-      this.setState({ currentPicture: 3 });
-    } else if (this.state.currentPicture === 3) {
-      this.setState({ currentPicture: 4 });
-    } else if (this.state.currentPicture === 4) {
-      this.setState({ currentPicture: 5 });
-    } else if (this.state.currentPicture === 5) {
-      this.setState({ currentPicture: 1 });
+    if (this.state.incrementor === this.pictures.length - 1) {
+      this.setState({ incrementor: 0 });
+    } else {
+      this.setState({ incrementor: this.state.incrementor + 1 });
     }
     this.timer = setInterval(() => {
-      return this.state.currentPicture === 1
-        ? this.setState({ currentPicture: 2 })
-        : this.state.currentPicture === 2
-          ? this.setState({ currentPicture: 3 })
-          : this.state.currentPicture === 3
-            ? this.setState({ currentPicture: 4 })
-            : this.state.currentPicture === 4
-              ? this.setState({ currentPicture: 5 })
-              : this.state.currentPicture === 5
-                ? this.setState({ currentPicture: 1 })
-                : 1;
+      if (this.state.incrementor === this.pictures.length - 1) {
+        this.setState({ incrementor: 0 });
+      } else {
+        this.setState({ incrementor: this.state.incrementor + 1 });
+      }
     }, 3000);
   }
 
-  handleDot1() {
-    return this.state.currentPicture === 1
-      ? 'fas fa-circle'
-      : 'far fa-circle';
+  handleDotsDisplayed() {
+    const dots = this.pictures.map((picture, index) =>
+      <div key={picture.id} className='circles'>
+        <i className={this.handleDotClassName(index)} onClick={() => this.handleDotClick(index)}></i>
+      </div>
+    );
+    return dots;
   }
 
-  handleDot2() {
-    return this.state.currentPicture === 2
-      ? 'fas fa-circle'
-      : 'far fa-circle';
-  }
-
-  handleDot3() {
-    return this.state.currentPicture === 3
-      ? 'fas fa-circle'
-      : 'far fa-circle';
-  }
-
-  handleDot4() {
-    return this.state.currentPicture === 4
-      ? 'fas fa-circle'
-      : 'far fa-circle';
-  }
-
-  handleDot5() {
-    return this.state.currentPicture === 5
-      ? 'fas fa-circle'
-      : 'far fa-circle';
-  }
-
-  handleDot1Click() {
+  handleDotClick(index) {
     clearInterval(this.timer);
-    this.setState({ currentPicture: 1 });
+    this.setState({ incrementor: index });
     this.timer = setInterval(() => {
-      return this.state.currentPicture === 1
-        ? this.setState({ currentPicture: 2 })
-        : this.state.currentPicture === 2
-          ? this.setState({ currentPicture: 3 })
-          : this.state.currentPicture === 3
-            ? this.setState({ currentPicture: 4 })
-            : this.state.currentPicture === 4
-              ? this.setState({ currentPicture: 5 })
-              : this.state.currentPicture === 5
-                ? this.setState({ currentPicture: 1 })
-                : 1;
+      if (this.state.incrementor === this.pictures.length - 1) {
+        this.setState({ incrementor: 0 });
+      } else {
+        this.setState({ incrementor: this.state.incrementor + 1 });
+      }
     }, 3000);
   }
 
-  handleDot2Click() {
-    clearInterval(this.timer);
-    this.setState({ currentPicture: 2 });
-    this.timer = setInterval(() => {
-      return this.state.currentPicture === 1
-        ? this.setState({ currentPicture: 2 })
-        : this.state.currentPicture === 2
-          ? this.setState({ currentPicture: 3 })
-          : this.state.currentPicture === 3
-            ? this.setState({ currentPicture: 4 })
-            : this.state.currentPicture === 4
-              ? this.setState({ currentPicture: 5 })
-              : this.state.currentPicture === 5
-                ? this.setState({ currentPicture: 1 })
-                : 1;
-    }, 3000);
-  }
-
-  handleDot3Click() {
-    clearInterval(this.timer);
-    this.setState({ currentPicture: 3 });
-    this.timer = setInterval(() => {
-      return this.state.currentPicture === 1
-        ? this.setState({ currentPicture: 2 })
-        : this.state.currentPicture === 2
-          ? this.setState({ currentPicture: 3 })
-          : this.state.currentPicture === 3
-            ? this.setState({ currentPicture: 4 })
-            : this.state.currentPicture === 4
-              ? this.setState({ currentPicture: 5 })
-              : this.state.currentPicture === 5
-                ? this.setState({ currentPicture: 1 })
-                : 1;
-    }, 3000);
-  }
-
-  handleDot4Click() {
-    clearInterval(this.timer);
-    this.setState({ currentPicture: 4 });
-    this.timer = setInterval(() => {
-      return this.state.currentPicture === 1
-        ? this.setState({ currentPicture: 2 })
-        : this.state.currentPicture === 2
-          ? this.setState({ currentPicture: 3 })
-          : this.state.currentPicture === 3
-            ? this.setState({ currentPicture: 4 })
-            : this.state.currentPicture === 4
-              ? this.setState({ currentPicture: 5 })
-              : this.state.currentPicture === 5
-                ? this.setState({ currentPicture: 1 })
-                : 1;
-    }, 3000);
-  }
-
-  handleDot5Click() {
-    clearInterval(this.timer);
-    this.setState({ currentPicture: 5 });
-    this.timer = setInterval(() => {
-      return this.state.currentPicture === 1
-        ? this.setState({ currentPicture: 2 })
-        : this.state.currentPicture === 2
-          ? this.setState({ currentPicture: 3 })
-          : this.state.currentPicture === 3
-            ? this.setState({ currentPicture: 4 })
-            : this.state.currentPicture === 4
-              ? this.setState({ currentPicture: 5 })
-              : this.state.currentPicture === 5
-                ? this.setState({ currentPicture: 1 })
-                : 1;
-    }, 3000);
+  handleDotClassName(index) {
+    if (this.state.incrementor === index) {
+      return 'fas fa-circle';
+    } else {
+      return 'far fa-circle';
+    }
   }
 
   render() {
     return (
-      <section id="container">
-        <div id="row">
-          <div id="column-third">
-            <i className="fas fa-angle-left" onClick={this.handleLeftAngle}></i>
+      <section className="container">
+        <div className="row">
+          <div className="column-third">
+            <div className='angle-Left'>
+              <i className="fas fa-angle-left" onClick={this.handleLeftAngle}></i>
+            </div>
           </div>
-          <div id="column-third">
-            <img src={this.handlePicture()}/>
+          <div className="column-third">
+            <div>
+              <img src={this.pictures[this.state.incrementor].picture} />
+              <div className="row">{this.handleDotsDisplayed()}</div>
+            </div>
           </div>
-          <div id="column-third">
-            <i className="fas fa-angle-right" onClick={this.handleRightAngle}></i>
+          <div className="column-third">
+            <div className='angle-Right'>
+              <i className="fas fa-angle-right" onClick={this.handleRightAngle}></i>
+            </div>
           </div>
         </div>
-        <div id="row">
-          <i className={this.handleDot1()} onClick={this.handleDot1Click}></i>
-          <i className={this.handleDot2()} onClick={this.handleDot2Click}></i>
-          <i className={this.handleDot3()} onClick={this.handleDot3Click}></i>
-          <i className={this.handleDot4()} onClick={this.handleDot4Click}></i>
-          <i className={this.handleDot5()} onClick={this.handleDot5Click}></i>
-        </div>
+
       </section>
     );
   }
 }
 
 export default Carousel;
+
+// import React from 'react';
+
+// class Carousel extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.pictures = props.pictures;
+//     this.state = {
+//       activeId: this.pictures[0].id
+//     };
+//     this.incrementor = 1;
+//     this.currentPicture = this.pictures[0].picture;
+//     this.timer = 0;
+//     this.handleRightAngle = this.handleRightAngle.bind(this);
+//     this.handleLeftAngle = this.handleLeftAngle.bind(this);
+//   }
+
+//   componentDidMount() {
+//     // this.timer = setInterval(() => {
+//     //   if (this.incrementor !== this.pictures.length) {
+//     //     this.setState({ activeId: this.pictures[this.incrementor].id });
+//     //     this.incrementor++;
+//     //   } else {
+//     //     this.incrementor = 0;
+//     //   }
+//     //   console.log('3 seconds.');
+//     // }, 3000);
+//   }
+
+//   handleLeftAngle() {
+//     clearInterval(this.timer);
+
+//     this.timer = setInterval(() => {
+
+//     }, 3000);
+//   }
+
+//   handleRightAngle() {
+//     clearInterval(this.timer);
+
+//     this.timer = setInterval(() => {
+//       return this.state.currentPicture === 1
+//         ? this.setState({ currentPicture: 2 })
+//         : this.state.currentPicture === 2
+//           ? this.setState({ currentPicture: 3 })
+//           : this.state.currentPicture === 3
+//             ? this.setState({ currentPicture: 4 })
+//             : this.state.currentPicture === 4
+//               ? this.setState({ currentPicture: 5 })
+//               : this.state.currentPicture === 5
+//                 ? this.setState({ currentPicture: 1 })
+//                 : 1;
+//     }, 3000);
+//   }
+
+//   handleDotsDisplayed() {
+//     const dots = this.pictures.map(picture =>
+//       <i key={picture.id} className={this.handleDotClassName(picture)}></i>
+//     );
+//     return dots;
+//   }
+
+//   handleDotClassName(pictureObject) {
+//     if (this.state.activeId === pictureObject.id) {
+//       this.currentPicture = pictureObject.picture;
+//       return 'fas fa-circle';
+//     } else {
+//       return 'far fa-circle';
+//     }
+//   }
+
+//   render() {
+//     console.log('this.currentPicture:', this.currentPicture);
+//     return (
+//       <section className="container">
+//         <div className="row">
+//           <div className="column-third">
+//             <i className="fas fa-angle-left" onClick={this.handleLeftAngle}></i>
+//           </div>
+//           <div className="column-third">
+//             <div>
+//             <img src={this.currentPicture}/>
+//             <div className="row">{this.handleDotsDisplayed()}</div>
+//             </div>
+//           </div>
+//           <div className="column-third">
+//             <i className="fas fa-angle-right" onClick={this.handleRightAngle}></i>
+//           </div>
+//         </div>
+
+//       </section>
+//     );
+//   }
+// }
+
+// export default Carousel;
